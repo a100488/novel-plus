97b6a5433d9da18fbc008fb149f006f1a64afff7
songanwei <songanwei@sinoiov.com>
2022-01-11 17:25:59 +0800

    feat: 小说url爬取4

--- novel-crawl/src/main/java/com/java2nb/novel/service/impl/B2FileBookContentServiceImpl.java
+++ novel-crawl/src/main/java/com/java2nb/novel/service/impl/B2FileBookContentServiceImpl.java
@@ -10,6 +10,7 @@ import com.java2nb.novel.service.BookContentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.dynamic.sql.render.RenderingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
@@ -28,26 +29,45 @@ public class B2FileBookContentServiceImpl implements BookContentService {
 
 
 
-
+    @Value("${content.save.b2path}")
+    private String fileSavePath;
+    @Value("${content.save.storage}")
+    private String storage;
+    @Autowired
+    private  B2FileUtil b2FileUtil;
 
     private final B2BookContentMq b2BookContentMq;
     @Override
     public void saveBookContent(List<BookContent> bookContentList,Long bookId) {
 
 
-        bookContentList.forEach(bookContent -> b2BookContentMq.producerBookContent(bookContent,bookId));
+        bookContentList.forEach(bookContent -> saveBookContent(bookContent,bookId));
 
     }
 
     @Override
     public void saveBookContent(BookContent bookContent,Long bookId) {
 
-        b2BookContentMq.producerBookContent(bookContent,bookId);
+
+        //消费逻辑
+        String fileSrc=bookId+"/"+bookContent.getIndexId()+".txt";
+        FileUtil.writeContentToFile(fileSavePath,fileSrc,bookContent.getContent());
+        File file = new File(fileSavePath + fileSrc);
+        try {
+            //上传到oss 减少磁盘空间
+            if (file.exists()) {
+                b2FileUtil.uploadSmallFile(file, fileSrc);
+                log.info("上传b2成功"+fileSrc);
+
+            }
+        }finally {
+            file.delete();
+        }
 
     }
 
     @Override
     public void updateBookContent(BookContent bookContent,Long bookId) {
-        b2BookContentMq.producerBookContent(bookContent,bookId);
+        saveBookContent(bookContent,bookId);
     }
 }
